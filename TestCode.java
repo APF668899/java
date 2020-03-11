7f784395cebfb5f399ed4453396ef099125d826f
APF668899 <59566818+APF668899@users.noreply.github.com>
2020-03-11 08:58:29 +0800

    测试代码

---
+++ TestCode.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class hello{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("请输入一个数");//输出
+        int a=in.nextInt();//输入
+        System.out.println(a);//输出
+    }
+}
