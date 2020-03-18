8d79cb81a3df877d8aca03b05718d79419ee2bdd
APF668899 <59566818+APF668899@users.noreply.github.com>
2020-03-18 10:47:49 +0800

    Create Shuchu.java

---
+++ Shuchu.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+	public static void prinone(){
+		System.out.println("方法一:");
+		System.out.println("520\n520\n520 1314");
+	}
+	public static void printwo(){
+		System.out.println("方法二:");
+		System.out.println("520");
+	    System.out.println("520");
+	    System.out.println("520 1314");
+	}
+	public tatic void prrinthree(){
+		System.out.print("方法三:");
+		System.out.print("\n520\n520\n520 1314");
+		System.out.print("\n双引号里面有什么输出什么、print输出完后不换行、println输出完后换行、“\ n”是换行");
+	}
+    public static void main(String []args) {
+       prinone();
+	   printwo();
+	   prrinthree();
+    }
+}
